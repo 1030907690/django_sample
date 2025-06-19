05e96199461ceff2615434ffde13c11dc1ac9f30
zzq <1030907690@qq.com>
2025-06-19 11:31:02 +0800

    feat:model，说明

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+from datetime import datetime
+
+
+# Create your models here.
+class Users(models.Model):
+    # id = models.AutoField(primary_key=True) #主键可省略不写
+    name = models.CharField(max_length=32)
+    age = models.IntegerField(default=20)
+    phone = models.CharField(max_length=16)
+    create_time = models.DateTimeField(default=datetime.now)
