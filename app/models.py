dfca3e68c5d66f18172645cf567cc236e7fe33f3
zzq <1030907690@qq.com>
2025-06-19 14:23:00 +0800

    feat:id

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 # Create your models here.
 class User(models.Model):
-    # id = models.AutoField(primary_key=True) #主键可省略不写
+    id = models.AutoField(primary_key=True) #主键可省略不写
     name = models.CharField(max_length=32)
     age = models.IntegerField(default=20)
     phone = models.CharField(max_length=16)
