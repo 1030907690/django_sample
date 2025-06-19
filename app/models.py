3b8c82d75a26087c2ed16c01f05f0e75a147e19a
zzq <1030907690@qq.com>
2025-06-19 13:38:56 +0800

    feat:自定义表名

--- app/models.py
+++ app/models.py
@@ -6,9 +6,12 @@ from datetime import datetime
 
 
 # Create your models here.
-class Users(models.Model):
+class User(models.Model):
     # id = models.AutoField(primary_key=True) #主键可省略不写
     name = models.CharField(max_length=32)
     age = models.IntegerField(default=20)
     phone = models.CharField(max_length=16)
     create_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        db_table = 't_user'  # 指定数据库表名
